36b2c43378510a1eb124153a97f23e7d73ddbcf7
SitekX1 <as@sitekx.de>
2026-07-08 21:25:50 +0200

    fix: maxNativeZoom setzen damit kein weißer Bildschirm beim Reinzoomen

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -219,13 +219,15 @@ export default function MapView({
           <TileLayer
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             attribution="© Esri"
-            maxZoom={19}
+            maxNativeZoom={19}
+            maxZoom={21}
           />
         ) : (
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            maxZoom={19}
+            maxNativeZoom={19}
+            maxZoom={21}
           />
         )}
 
